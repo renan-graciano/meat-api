87bd7601f8748c20dee1f7bcfa4eb7e572e9e7fe
Renan Graciano <renangraciano@MacBook-Air-de-Renan.local>
2020-06-22 23:53:45 -0300

    Inclusão de métodos banco

--- dist/server/server.js
+++ dist/server/server.js
@@ -4,6 +4,7 @@ exports.Server = void 0;
 const restify = require("restify");
 const mongoose = require("mongoose");
 const environment_1 = require("../common/environment");
+const merge_patch_parser_1 = require("./merge-patch.parser");
 class Server {
     initializeDb() {
         mongoose.Promise = global.Promise;
@@ -20,6 +21,7 @@ class Server {
                 });
                 this.application.use(restify.plugins.queryParser());
                 this.application.use(restify.plugins.bodyParser());
+                this.application.use(merge_patch_parser_1.mergePatchBodyParser);
                 //routes
                 for (let router of routers) {
                     router.applyRoutes(this.application);

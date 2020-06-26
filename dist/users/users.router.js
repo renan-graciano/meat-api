967e94c2f91f0749e37f496b57a351efd82527d9
Renan Graciano <renangraciano@MacBook-Air-de-Renan.local>
2020-06-25 23:50:34 -0300

    criando validações e hashPasswors

--- dist/users/users.router.js
+++ dist/users/users.router.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.usersRouter = void 0;
 const router_1 = require("../common/router");
+const restify_errors_1 = require("restify-errors");
 const users_model_1 = require("../users/users.model");
 class UsersRouter extends router_1.Router {
     constructor() {
@@ -13,18 +14,23 @@ class UsersRouter extends router_1.Router {
     applyRoutes(application) {
         application.get('/users', (req, resp, next) => {
             //resp.json({message: 'users router'})
-            users_model_1.User.find().then(this.render(resp, next));
+            users_model_1.User.find()
+                .then(this.render(resp, next))
+                .catch(next);
         });
         application.get('/users/:id', (req, resp, next) => {
             users_model_1.User.findById(req.params.id)
-                .then(this.render(resp, next));
+                .then(this.render(resp, next))
+                .catch(next);
         });
         application.post('/users', (req, resp, next) => {
             let user = new users_model_1.User(req.body);
-            user.save().then(this.render(resp, next));
+            user.save()
+                .then(this.render(resp, next))
+                .catch(next);
         });
         application.put('/users/:id', (req, resp, next) => {
-            const options = { overwrite: true };
+            const options = { runValidators: true, overwrite: true };
             users_model_1.User.update({ _id: req.params.id }, req.body, options)
                 .exec().then(result => {
                 if (result.n) {
@@ -34,14 +40,16 @@ class UsersRouter extends router_1.Router {
                     });
                 }
                 else {
-                    resp.send(404);
+                    throw new restify_errors_1.NotFoundError('Documento não encontrado');
                 }
-            }).then(this.render(resp, next));
+            }).then(this.render(resp, next))
+                .catch(next);
         });
         application.patch('/users/:id', (req, resp, next) => {
-            const options = { new: true };
+            const options = { runValidators: true, new: true };
             users_model_1.User.findByIdAndUpdate(req.params.id, req.body, options)
-                .then(this.render(resp, next));
+                .then(this.render(resp, next))
+                .catch(next);
         });
         application.del('/users/:id', (req, resp, next) => {
             users_model_1.User.remove({ _id: req.params.id }).exec().then((cmdResult) => {
@@ -49,10 +57,10 @@ class UsersRouter extends router_1.Router {
                     resp.send(204);
                 }
                 else {
-                    resp.send(404);
+                    throw new restify_errors_1.NotFoundError('Documento não encontrado');
                 }
                 return next();
-            });
+            }).catch(next);
         });
     }
 }
--- users/users.router.ts
+++ users/users.router.ts
@@ -1,7 +1,8 @@
 import { Router } from '../common/router'
 import * as restify from 'restify'
-import {User} from '../users/users.model'
-import { networkInterfaces } from 'os';
+import { BadRequestError, NotFoundError } from 'restify-errors'
+import { User } from '../users/users.model'
+
 
 class UsersRouter extends Router{
 
@@ -16,21 +17,26 @@ class UsersRouter extends Router{
 
     application.get('/users', (req, resp, next) => {
       //resp.json({message: 'users router'})
-      User.find().then(this.render(resp, next))
+      User.find()
+        .then(this.render(resp, next))
+        .catch(next)
     })
 
     application.get('/users/:id', (req,resp,next) => {
       User.findById(req.params.id)
         .then(this.render(resp, next))
+        .catch(next)
     })
 
     application.post('/users', (req, resp, next) => {
       let user = new User(req.body)
-      user.save().then(this.render(resp,next))
+      user.save()
+        .then(this.render(resp, next))
+        .catch(next)
     })
 
     application.put('/users/:id', (req, resp, next) => {
-      const options = {overwrite: true}
+      const options = {runValidators: true, overwrite: true}
       User.update({ _id: req.params.id }, req.body, options)
         .exec().then(result => {
           if (result.n) {
@@ -40,15 +46,17 @@ class UsersRouter extends Router{
             })
           }
           else {
-            resp.send(404)
+            throw new NotFoundError('Documento não encontrado')
           }
-        }).then(this.render(resp,next))
+        }).then(this.render(resp, next))
+        .catch(next)
     })
 
     application.patch('/users/:id', (req, resp, next) => {
-      const options = {new: true}
+      const options = {runValidators: true, new: true}
       User.findByIdAndUpdate(req.params.id, req.body, options)
-        .then(this.render(resp,next))
+        .then(this.render(resp, next))
+        .catch(next)
     })
 
     application.del('/users/:id', (req, resp, next) => {
@@ -56,10 +64,10 @@ class UsersRouter extends Router{
         if (cmdResult.result.n) {
           resp.send(204)
         } else {
-          resp.send(404)
+          throw new NotFoundError('Documento não encontrado')
         }
         return next()
-      })
+      }).catch(next)
     })
   }
 }

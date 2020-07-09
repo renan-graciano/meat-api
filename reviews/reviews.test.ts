1bf9dbb647d972cae74ae3097466e9b5d6604e6f
Renan Graciano <renangraciano@MacBook-Air-de-Renan.local>
2020-07-09 00:15:05 -0300

    correção de testess

--- reviews/reviews.test.ts
+++ reviews/reviews.test.ts
@@ -25,10 +25,29 @@ test('teste get /reviews/aaaa', () => {
     })
   .catch(fail)
 })
-
+/* test('teste post /reviews', () => {
+  return request(address)
+    .post('/reviews')
+    .send({
+      date: '2018-02-02T20:20:20',
+      rating: 4,
+      comments: 'ok',
+      user: new mongoose.Types.ObjectId(),
+      restaurant: new mongoose.Types.ObjectId()
+    })
+    .then(response => {
+      expect(response.status).toBe(200)
+      expect(response.body._id).toBeDefined()
+      expect(response.body.rating).toBe(4)
+      expect(response.body.comments).toBe('ok')
+      expect(response.body.user).toBeDefined()
+      expect(response.body.restaurant).toBeDefined()
+    })
+    .catch(fail)
+}) */
 test('teste post /reviews', () => {
-  let userid: mongoose.Types.ObjectId
-  let restaurantid: mongoose.Types.ObjectId
+  let userid = mongoose.Types.ObjectId
+  let restaurantid = mongoose.Types.ObjectId
   return request(address)
     .post('/users')
     .set('Authorization', auth)
@@ -49,6 +68,7 @@ test('teste post /reviews', () => {
           restaurantid = response.body._id
           request(address)
             .post('/reviews')
+            .set('Authorization', auth)
             .send({
               date: "2020-06-27T17:59:12",
               rating: 7,
